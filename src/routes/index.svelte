e50c1433b06618acb4d840fb3b8f0399aa871a7b
Johann <johann.taberlet@gmail.com>
2024-08-30 02:18:31 +0200

    Test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,12 @@
 <img src="/images/En-construction-error-503.jpg" alt="" />
+<div class="text-center">
+	<h1>Site en construction</h1>
+	<p>
+		Nous sommes en train de mettre au point notre nouveau site web. Nous sommes désolés pour la gêne
+		occasionnée.
+	</p>
+	<p>Nous vous invitons à revenir plus tard.</p>
+</div>
 
 <style>
 	img {
